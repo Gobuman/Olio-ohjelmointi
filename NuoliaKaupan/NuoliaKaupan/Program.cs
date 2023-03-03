e420630c41d2a8214fcc91a708cd7825f73284cf
gabrielamose <s2100944@edu.bc.fi>
2023-03-03 12:16:04 +0200

    added nuolikauppa

---
+++ NuoliaKaupan/NuoliaKaupan/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+string karkitilaus = "a";
+string peratilaus = "b";
+int pituustilaus = 0;
+string haluttupituus;
+
+Console.WriteLine("Minkälainen kärki (puu, teräs, timantti) :");
+while (karkitilaus != "puu" || karkitilaus != "teräs" || karkitilaus != "timantti")
+{
+    karkitilaus = Console.ReadLine();
+    if (karkitilaus == "puu" || karkitilaus == "timantti" || karkitilaus == "teräs")
+    {
+        break;
+    }
+}
+Console.WriteLine("Minkälainen perä (lehti, kanansulka, kotkansulka) :");
+while (peratilaus != "lehti" || peratilaus != "kanansulka" || peratilaus != "kotkansulka")
+{
+    peratilaus = Console.ReadLine();
+    if (peratilaus == "lehti" || peratilaus == "kanansulka" || peratilaus == "kotkansulka")
+    {
+        break;
+    }
+}
+Console.WriteLine("Nuolen pituus (60-100cm) :");
+while (pituustilaus < 60 || pituustilaus > 100)
+{
+    haluttupituus = Console.ReadLine();
+    if (int.TryParse(haluttupituus, out pituustilaus) == true && pituustilaus < 100 && pituustilaus > 60)
+    {
+        break;
+    }
+}
+Nuoli tilattuNuoli = new Nuoli(karkitilaus, peratilaus, pituustilaus);
+Console.WriteLine("Nuoli maksaa " + tilattuNuoli.PalautaHinta + " kultaa");
+
+
+
+public class Nuoli
+{
+    private string _karki;
+    private string _pera;
+    private double _pituus;
+    private double nuolenhinta;
+
+    public Nuoli(string karki, string pera, int pituus)
+    {
+        _karki = karki;
+        _pera = pera;
+        _pituus = pituus;
+        if (_karki == "puu")
+        {
+            nuolenhinta += 3;
+        }
+        if (_karki == "teräs")
+        {
+            nuolenhinta += 5;
+        }
+        if (_karki == "timantti")
+        {
+            nuolenhinta += 50;
+        }
+        if (_pera == "kanansulka")
+        {
+            nuolenhinta += 1;
+        }
+        if (_pera == "kotkansulka")
+        {
+            nuolenhinta += 5;
+        }
+        nuolenhinta = nuolenhinta + _pituus * 0.05;
+        return;
+    }
+
+    public double PalautaHinta
+    {
+        get { return nuolenhinta; }
+        set { nuolenhinta = value; }
+    }
+    public string Karki
+    {
+        get { return _karki; }
+        set { _karki = value; }
+    }
+    public string Pera
+    {
+        get { return _pera; }
+        set { _pera = value; }   
+    }
+    public double Pituus
+    {
+        get { return _pituus; }
+        set { _pituus = value; }
+    }
+}
